86f9c43ecc96050cfbd3e336117da8620f3eb911
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-05-28 08:41:06 -0600

    progress with my arrays

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,20 +1,72 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
-	private FoodTruck[] fleet;
+	private static FoodTruck[] fleet;
 
 	public static void main(String[] args) {
 		FoodTruckApp app = new FoodTruckApp();
-		app.run();
-	
-	
+		Scanner scan = new Scanner(System.in);
+//		app.run();
+
+		fleet = new FoodTruck[5];
+
+		for (int truckNum = 0; truckNum < fleet.length; truckNum++) {
+			System.out.println("What is the food truck name? (Enter 'quit' to stop entering trucks)");
+			String truckName = scan.nextLine();
+			if (truckName.equalsIgnoreCase("quit")) {
+				break;
+			}
+			System.out.println("What type of food does the truck serve?");
+			String truckType = scan.nextLine();
+			System.out.println("What is the trucks rating?");
+			double truckRating = scan.nextDouble();
+			FoodTruck truck = new FoodTruck(truckName, truckType, truckRating);
+			fleet[truckNum] = truck;
+			scan.nextLine();
+		}
+		
+		app.menu();
+		
+		
+		
+		
+
+	}
+
+	public void menu() {
+		System.out.println("****************** MENU ******************");
+		System.out.println("1. List all existing food trucks");
+		System.out.println("2. See the average rating of food trucks");
+		System.out.println("3. Display the highest-rated food truck");
+		System.out.println("4. Quit the program");
 	}
 	
-	public void run() {
+	public double averageRating(double trucksum) {
+		double sum = 0;
+		double avg = 0;
+		for (FoodTruck truckRating : fleet) {
+			sum += trucksum;
+		}
+		avg = sum / fleet.length;
+		return avg;
+		
 	}
+	
+	
 	
 	
 
+	@Override
+	public String toString() {
+		return "FoodTruckApp [fleet=" + Arrays.toString(fleet) + "]";
+	}
+
 }
+
+//	public void run() {
+//	}
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,35 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
+private static int currentNumberOfTrucks;// everytime constructor is called this must be incrememnted by 1 and assign this number to be the current Id
+private int id; 
+
+private String name;
+private String foodType;
+private double rating;
+
+
+
+
+
+public FoodTruck() {
+	FoodTruck.currentNumberOfTrucks++;
+	id = FoodTruck.currentNumberOfTrucks;
+}
+
+
+public FoodTruck (String name, String foodType, double rating) {
+	this.name = name;
+	this.foodType = foodType;
+	this.rating = rating;
+	
+}
+
+
+
+
+
+
+
 
 }
