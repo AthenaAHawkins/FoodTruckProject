8ab5d7a0be86dac48ec0f61e509b4329817baec9
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-05-30 07:55:52 -0600

    readme

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # FoodTruckProject
 
 #Description
+This project was quite difficult for me. I felt like I struggled through it one method at a time but we did it! 
+
+I started off writing my for loop and used that to iterate through the food truck information gatherer. From there, I decided to work on my methods and how to get the information from my constructors / getters and setters. This by far for me was the most challenging part. I had a semi grasp on how these function however when I started writing I realized that I was struggling with it a lot more than I thought. I worked with a fellow classmate and they were able to help provide some guidance and then we worked on the methods. These took us 6 hours to work through. But we did it! This was the best way for me to learn how everything worked together honestly. my methods work in similar ways to each other. They iterate through each food truck and if there's a field that returns null, will stop iterating. I struggled with how to get highest average to return the proper amount. 
+
 
 
 #Technologies used
+methods();
+arrays[];
+constructors();
 
 
 #lessons learned
-
+leave lots of time for coding and there's power in numbers. I was lucky to have someone to work on this class with to throw our heads together and work through the problems together.  
 
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -7,7 +7,7 @@ import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
 	private FoodTruck[] fleet = new FoodTruck[5];
-	private int currentNumberOfTrucks=0;
+	private int currentNumberOfTrucks = 0;
 
 	public void run() {
 		Scanner scan = new Scanner(System.in);
@@ -28,17 +28,14 @@ public class FoodTruckApp {
 			FoodTruck truck = new FoodTruck(truckName, truckType, truckRating);
 			fleet[truckNum] = truck;
 			currentNumberOfTrucks++;
-			
 
 		}
 		boolean askAgain = true;
-		while(askAgain)
-		{
+		while (askAgain) {
 			menu();
 			int choice = scan.nextInt();
 			switch (choice) {
 			case 1:
-				System.out.println("All trucks currently stached are: ");
 				allExistingFoodTrucks();
 				break;
 			case 2:
@@ -56,22 +53,24 @@ public class FoodTruckApp {
 				System.out.println("Improper input, try again");
 			}
 		}
+		scan.close();
 	}
 
 	public void menu() {
-
+		System.out.println("");
 		System.out.println("****************** MENU ******************");
 		System.out.println("1. List all existing food trucks");
 		System.out.println("2. See the average rating of food trucks");
 		System.out.println("3. Display the highest-rated food truck");
 		System.out.println("4. Quit the program");
-	
+		System.out.println("");
+
 	}
 
 	public double averageRating() {
 		double sum = 0;
 		double avg = 0;
-		for (int index = 0; index < fleet.length; index ++) {
+		for (int index = 0; index < fleet.length; index++) {
 			if (fleet[index] != null) {
 				sum += fleet[index].getRating();
 			}
@@ -83,21 +82,21 @@ public class FoodTruckApp {
 
 	public void allExistingFoodTrucks() {
 		System.out.println("All trucks currently stored are: ");
-		for(int index = 0; index < fleet.length; index ++) {
-			if( fleet[index] != null ) {
+		for (int index = 0; index < fleet.length; index++) {
+			if (fleet[index] != null) {
 				System.out.println(fleet[index].toString());
 			}
 		}
-			
-		}
-		
+
+	}
+
 	public double highestRated() {
 		double maxNum = 0;
 		for (int index = 0; index < fleet.length; index++) {
-			if(fleet[index] != null) {
-			if (fleet[index].getRating() > maxNum) {
-			maxNum = fleet[index].getRating();
-		}
+			if (fleet[index] != null) {
+				if (fleet[index].getRating() > maxNum) {
+					maxNum = fleet[index].getRating();
+				}
 			}
 		}
 		return maxNum;
